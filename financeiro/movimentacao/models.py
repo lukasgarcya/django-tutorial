3c14c517df3451b54b90772baacbd24801f44c84
Lucas Garcia de Araujo <lucas.araujo@ifgoiano.edu.br>
2016-04-28 17:41:54 -0300

    Criação do modelo para Transacao.

--- financeiro/movimentacao/models.py
+++ financeiro/movimentacao/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
+
+class Transacao(models.Model):
+    data_hora = models.DateTimeField()
+    descricao = models.CharField(max_length=255)
+    categoria = models.ForeignKey(Categoria)
+    valor = models.DecimalField(max_digits=11, decimal_places=2)
